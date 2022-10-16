2052e220258f4666e933cd5e6620ab88562f30a0
eslam-eng <76536997+eslam-eng@users.noreply.github.com>
2022-10-17 01:07:00 +0200

    add user name to users table

--- database/migrations/2022_09_22_000000_create_users_table.php
+++ database/migrations/2022_09_22_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('user_name');
+            $table->string('email')->nullable()->unique();
             $table->string('password');
             $table->string('phone')->unique();
             $table->tinyInteger('type')->default(\App\Models\User::CUSTOMERTYPE);
