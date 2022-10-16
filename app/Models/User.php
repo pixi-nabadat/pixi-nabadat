7501b0c8aebe74689823cd1dfc55774a0c452070
eslam-eng <76536997+eslam-eng@users.noreply.github.com>
2022-10-17 00:44:58 +0200

    add location eager load with users

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
         return $this->createToken(config('app.name'))->plainTextToken;
     }
 
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
 }
