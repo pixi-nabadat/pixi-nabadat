df8c50951bd579464f688e80ee573e56c2b6ac72
mohamed hamada <mh3532016@gmail.com>
2022-11-20 00:22:35 +0200

    booking database tables

---
+++ database/migrations/2022_11_19_213548_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignIdFor(\App\Models\Center::class)->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->date('booking_date');
+            $table->date('confirm_date')->nullable();
+            $table->foreignId('confirm_user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->foreignId('attend_user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->enum('booking_status', ['pending', 'confirmed', 'completed', 'expired'])->nullable();
+            $table->enum('payment_type', ['cash', 'card'])->nullable();
+            $table->boolean('payment_satus')->default(false);
+            $table->string('qr_code');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
---
+++ database/migrations/2022_11_19_221010_booking_devices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_devices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->references('id')->on('bookings')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('device_id')->references('id')->on('center_devices')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->integer('palses_number');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking_devices');
+    }
+};
