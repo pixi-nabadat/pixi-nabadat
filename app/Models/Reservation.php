d1dedd314c28c9309a393346b558bb412d8edef0
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2022-11-24 20:38:07 +0200

    reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Reservation extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
+    protected static $pending   = ['en'=>'pending',   'ar'=>'قيد الانتظار'];
+    protected static $confirmed = ['en'=>'confirmed', 'ar'=>'تم التأكيد'];
+    protected static $attend    = ['en'=>'attend',    'ar'=>'تم الحضور'];
+    protected static $completed = ['en'=>'completed', 'ar'=>'مكتمل'];
+    protected static $canceled  = ['en'=>'canceled',  'ar'=>'تم الإلغاء'];
+    protected static $expired   = ['en'=>'expired',   'ar'=>'تالف'];
+
+    protected $fillable  = [
         'customer_id',
         'center_id',
         'confirm_date',
@@ -19,4 +25,42 @@ class Reservation extends Model
         'qr_code',
     ];
 
+    public static function pending(string $lang){
+        return Reservation::$pending[$lang];
+    }
+    public static function confirm(string $lang){
+        return Reservation::$confirmed[$lang];
+    }
+    public static function attend(string $lang){
+        return Reservation::$attend[$lang];
+    }
+    public static function completed(string $lang){
+        return Reservation::$completed[$lang];
+    }
+    public static function expired(string $lang){
+        return Reservation::$expired[$lang];
+    }
+    public static function canceled(string $lang){
+        return Reservation::$canceled[$lang];
+    }
+
+    public function history()
+    {
+        return $this->hasMany(ReservationHistory::class,'reservation_id');
+    }
+
+    public function nabadatHistory()
+    {
+        return $this->hasMany(NabadatHistory::class,'reservation_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'customer_id');
+    }
+
+    public function center()
+    {
+        return $this->belongsTo(Center::class);
+    }
 }
