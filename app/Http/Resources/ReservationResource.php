d443bf9326610763180a7cfcf6a540fe0e8a71c7
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2022-11-24 20:37:49 +0200

    other reservatio resources

--- app/Http/Resources/ReservationResource.php
+++ app/Http/Resources/ReservationResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\CentersResource;
+use Carbon\Carbon;
+
 class ReservationResource extends JsonResource
 {
     /**
@@ -15,14 +17,14 @@ class ReservationResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'customer'=> AuthUserResource::collection($this->customer_id),
-            'center'=>CentersResource::collection($this->center),
-            'reservation_date'=> $this->reservation_date,
-            'check_date'=> $this->check_date,
-            'reservation_status'=> $this->reservation_status,
-            'payment_type'=> $this->payment_type,
-            'payment_status'=> $this->payment_status,
-            'qr_code'=> $this->qr_code,
+            'check_date'     => $this->check_date,
+            'check_day'      => Carbon::parse($this->check_date)->dayName,
+            'payment_type'   => $this->payment_type,
+            'payment_status' => $this->payment_status,
+            'qr_code'        => $this->qr_code,
+            'customer'       => $this->whenLoaded('user') ? new AuthUserResource($this->user):[],
+            'center'         => $this->whenLoaded('center') ? new CentersResource($this->center):[],
+            'status'         => $this->whenLoaded('history') ? new ReservationHistoryResource($this->history->last()):[],
         ];
     }
 }
