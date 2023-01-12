f1acbfc452b7257489d2394c4bff479da7af3f89
eslam-eng <76536997+eslam-eng@users.noreply.github.com>
2023-01-12 14:33:19 +0200

    add reservation to dashboard

---
+++ resources/views/dashboard/reservations/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.simple.master')
+
+@section('title', trans('lang.reservations'))
+
+@section('breadcrumb-title')
+    <h3>{{ trans('lang.reservations') }}</h3>
+@endsection
+
+@section('breadcrumb-items')
+    <li class="breadcrumb-item"><a href="{{route('home')}}">{{ trans('lang.dashboard') }}</a></li>
+    <li class="breadcrumb-item active">{{ trans('lang.reservations') }}</li>
+@endsection
+
+@section('content')
+
+    <!-- Container-fluid starts-->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Zero Configuration  Starts-->
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table">
+                            {!! $dataTable->table() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Zero Configuration  Ends-->
+        </div>
+    </div>
+    <!-- Container-fluid Ends-->
+
+@endsection
+
+@section('script')
+{!! $dataTable->scripts() !!}
+@endsection
