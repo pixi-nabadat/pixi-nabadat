4059b1fa0b9b64d0fcb2bff33ff1a48b28576bc4
eslam-eng <76536997+eslam-eng@users.noreply.github.com>
2023-01-15 00:28:39 +0200

    update center device namespace

--- app/Services/CenterDeviceService.php
+++ app/Services/CenterDeviceService.php
@@ -2,17 +2,16 @@
 
 namespace App\Services;
 
-use App\Models\centerDevice;
-use App\Models\User;
+use App\Models\CenterDevice;
 use App\QueryFilters\CenterDevicesFilter;
 use Illuminate\Database\Eloquent\Builder;
 
 class CenterDeviceService extends BaseService
 {
 
-    public function getAll(array $where_condition = [], array $withRelation=[])
+    public function getAll(array $where_condition = [], array $withRelation = [])
     {
-        return $this->queryGet($where_condition,$withRelation)->get();
+        return $this->queryGet($where_condition, $withRelation)->get();
     }
 
     public function queryGet(array $where_condition = [], $withRelation = []): Builder
@@ -21,17 +20,6 @@ class CenterDeviceService extends BaseService
         return $centerDevices->filter(new CenterDevicesFilter($where_condition));
     }
 
-    public function find(int $id)
-    {
-
-        $centerDevice = CenterDevice::find($id);
-        if ($centerDevice)
-            return $centerDevice;
-        return false;
-
-    } //end of find
-
-
     public function update(int $id, array $data = []): bool
     {
         $centerDevice = $this->find($id);
@@ -39,5 +27,15 @@ class CenterDeviceService extends BaseService
             $centerDevice->update($data);
         }
         return false;
+    } //end of find
+
+public function find(int $id)
+    {
+
+        $centerDevice = CenterDevice::find($id);
+        if ($centerDevice)
+            return $centerDevice;
+        return false;
+
     } //end of update
 }
