3d6e149f22ed270eef00b75cf86628823c02766a
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-01-15 22:55:39 +0200

    add target for app link

--- resources/views/referal.blade.php
+++ resources/views/referal.blade.php
@@ -19,7 +19,7 @@
       <div class="col-md-8 offset-md-2">
         <p class="sub-content">Download the app and get more points you can use in purchasing products and more</p>
       </div>
-      <div><a class="btn btn-danger-gradien btn-lg" href="{{route('home')}}">Download App</a></div>
+      <div><a class="btn btn-danger-gradien btn-lg" href="{{route('home')}}" target="_blank">Download App</a></div>
     </div>
   </div>
   <!-- error-404 end      -->
