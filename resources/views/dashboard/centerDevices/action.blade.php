8fa5f2a7290e25307480b644ec88ced8ea125ff6
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-02-19 22:18:55 +0200

    remove center devices crud from dashboard

--- resources/views/dashboard/centerDevices/action.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="d-flex justify-content-center">
-
-    @can('edit_center_device')
-    <a href="{{ route('centerDevices.edit', $centerDevice) }}" class="btn-sm btn-info me-1">
-        <i class="fa fa-pencil-square-o  my-2"></i>
-    </a>
-    @endcan
-</div>
