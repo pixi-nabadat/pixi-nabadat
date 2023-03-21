6013db94709d77c230683f6baae0a34966b46b29
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-03-21 20:18:10 +0200

    add fincance to center home page

--- app/Http/Controllers/Api/CenterHomeController.php
+++ app/Http/Controllers/Api/CenterHomeController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\FinanceResource;
 use App\Http\Resources\ReservationsResource;
+use App\Models\Invoice;
 use App\Models\Reservation;
+use App\Services\InvoiceService;
 use App\Services\ReservationService;
 use Illuminate\Support\Facades\Cache;
 
 class CenterHomeController extends Controller
 {
-    public function __construct(protected ReservationService $reservationService)
+    public function __construct(protected ReservationService $reservationService, protected InvoiceService $invoiceService)
     {
     }
 
@@ -20,6 +23,7 @@ class CenterHomeController extends Controller
         $centerId = auth('sanctum')->user()->center_id;
         $data = Cache::remember('center-home-api', 60 * 60 * 24, function () use ($centerId) {
             $data ['upcoming_reservations'] = ReservationsResource::collection($this->reservationService->queryGet(where_condition: ['center_id' => $centerId, 'status' => Reservation::PENDING], withRelation: ['user.attachments', 'latestStatus'])->orderByDesc('id')->limit(8)->get());
+            $data ['finance'] = new FinanceResource($this->invoiceService->queryGet(filters: ['center_id' => $centerId, 'status' => Invoice::PENDING], withRelations: ['transactions'])->first());
             return $data;
         });
         return apiResponse(data: $data);
