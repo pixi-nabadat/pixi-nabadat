035bd3c2ef6f7297d683afc07db92a29070c0857
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-03-23 22:38:52 +0200

    users table make email nullable

--- database/migrations/2022_09_22_000000_create_users_table.php
+++ database/migrations/2022_09_22_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('password');
             $table->string('phone')->unique();
             $table->tinyInteger('type')->default(\App\Models\User::CUSTOMERTYPE);
