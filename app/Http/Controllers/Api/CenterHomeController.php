057b904574cd4a1199570152a3da081a8f0c7ad0
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-03-31 00:55:43 +0200

    center home

--- app/Http/Controllers/Api/CenterHomeController.php
+++ app/Http/Controllers/Api/CenterHomeController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\FinanceResource;
 use App\Http\Resources\ReservationsResource;
+use App\Models\Invoice;
 use App\Models\Reservation;
+use App\Services\InvoiceService;
 use App\Services\ReservationService;
 use Illuminate\Support\Facades\Cache;
 
 class CenterHomeController extends Controller
 {
-    public function __construct(protected ReservationService $reservationService)
+    public function __construct(protected ReservationService $reservationService, protected InvoiceService $invoiceService)
     {
     }
 
@@ -20,7 +23,9 @@ class CenterHomeController extends Controller
         $data=[];
         $centerId = auth('sanctum')->user()->center_id;
         $data ['upcoming_reservations'] = ReservationsResource::collection($this->reservationService->queryGet(where_condition: ['center_id' => $centerId, 'status_in' => [Reservation::PENDING,Reservation::APPROVED]], withRelation: ['user.attachments', 'latestStatus'])->orderBy('id')->limit(10)->get());
-//        return $data;
+        $data ['finance'] = new FinanceResource($this->invoiceService->queryGet(filters: ['center_id' => $centerId, 'status' => Invoice::PENDING], withRelations: ['transactions'])->first());
+        $data ['done_appointments'] =  $this->reservationService->queryGet(where_condition: ['center_id' => $centerId, 'status' => Reservation::COMPLETED])->count();
+        //        return $data;
 //
 //        $data = Cache::remember('center-home-api', 60 * 60 * 24, function () use ($centerId) {
 //            $data ['upcoming_reservations'] = ReservationsResource::collection($this->reservationService->queryGet(where_condition: ['center_id' => $centerId, 'status' => Reservation::PENDING], withRelation: ['user.attachments', 'latestStatus'])->orderByDesc('id')->limit(8)->get());
