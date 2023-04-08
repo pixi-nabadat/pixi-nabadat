4a7d95390cc38654307e395d233d4cf32997f630
eslam <elbadry693@gmail.com>
2023-04-08 23:52:53 +0200

    fix order calculation

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -17,7 +17,8 @@ class OrderResource extends JsonResource
         return [
             'id'=>$this->id,
             'sub_total'=>$this->sub_total,
-            'grand_total'=>$this->grand_total,
+            'shipping_fees'=>$this->shipping_fees,
+            'grand_total'=>$this->grand_total + $this->shipping_fees,
             'discount'=>$this->coupon_discount,
             'status'=>$this->whenLoaded('latestStatus',$this->latestStatus->status_text),
             'items'=>$this->whenLoaded('items',OrderItemResource::collection($this->items))
