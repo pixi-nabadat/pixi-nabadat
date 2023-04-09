39604fbdd171c74eed9f77fc4b672007083f3261
eslam <elbadry693@gmail.com>
2023-04-10 01:57:50 +0200

    images in packages

--- app/Http/Resources/PackagesResource.php
+++ app/Http/Resources/PackagesResource.php
@@ -15,6 +15,7 @@ class PackagesResource extends JsonResource
      */
     public function toArray($request)
     {
+        $default_image = ['path'=>asset('assets/images/default-image.jpg')] ;
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -22,7 +23,7 @@ class PackagesResource extends JsonResource
             'price' => $this->price,
             'price_after_discount' => getPriceAfterDiscount(price: $this->price, discountValue: $this->discount_percentage ?? 0),
             // 'image' => $this->image_path,
-            'image'=> $this->whenLoaded(relationship: 'attachments',value: isset($this->attachments) ? new AttachmentsResource($this->attachments):['path'=>asset('assets/images/default-image.jpg')]),
+            'image'=> $this->whenLoaded(relationship: 'attachments',value: isset($this->attachments) ? new AttachmentsResource($this->attachments):$default_image,default: $default_image),
             'center' => new CentersResource($this->whenLoaded('center')),
         ];
     }
