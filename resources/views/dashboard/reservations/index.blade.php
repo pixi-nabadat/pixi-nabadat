91224dca1a8dc5dc91cb2bea7fdf7af16ea24fe4
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-04-25 01:37:14 +0200

    reservation filters

--- resources/views/dashboard/reservations/index.blade.php
+++ resources/views/dashboard/reservations/index.blade.php
@@ -16,6 +16,42 @@
     <!-- Container-fluid starts-->
     <div class="container-fluid">
         <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form class="needs-validation datatables_parameters" novalidate="">
+                            <div class="row g-3">
+                                <div class="col-md-4">
+                                    <label class="form-label" for="validationCustom01">@lang('lang.status')</label>
+                                    <select class="form-select" name="status" id="validationCustom01">
+                                        <option value="" selected>Choose...</option>
+                                        <option value="{{ App\Models\Reservation::PENDING }}">{{ trans('lang.pending') }}</option>
+                                        <option value="{{ App\Models\Reservation::APPROVED }}">{{ trans('lang.approved') }}</option>
+                                        <option value="{{ App\Models\Reservation::CONFIRMED }}">{{ trans('lang.confirmed') }}</option>
+                                        <option value="{{ App\Models\Reservation::ATTEND }}">{{ trans('lang.attend') }}</option>
+                                        <option value="{{ App\Models\Reservation::COMPLETED }}">{{ trans('lang.completed') }}</option>
+                                        <option value="{{ App\Models\Reservation::CANCELED }}">{{ trans('lang.canceled') }}</option>
+                                        <option value="{{ App\Models\Reservation::Expired }}">{{ trans('lang.expired') }}</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4 mb-3">
+                                    <label class="form-label" for="validationCustom02">@lang('lang.check_date')</label>
+                                    <input class="form-control" name="check_date" id="validationCustom02" type="date" value="">
+                                    <div class="valid-feedback">Looks good!</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <button class="btn btn-primary search_datatable" type="submit">{{trans('lang.search')}}</button>
+                                </div>
+                                <div class="col-md-3">
+                                    <button class="btn btn-danger reset_form_data" type="button">{{trans('lang.rest')}}</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
             <!-- Zero Configuration  Starts-->
             <div class="col-sm-12">
                 <div class="card">
@@ -24,7 +60,7 @@
                             <a role="button" class="btn btn-success" href="{{ route('reservations.create')}}"><i class="fa fa-plus-circle"></i> {{ trans('lang.add_reservation')}}</a>
                         </div>
                         <div class="table">
-                            {!! $dataTable->table() !!}
+                            {!! $dataTable->table(['class'=>'table-data']) !!}
                         </div>
                     </div>
                 </div>
@@ -37,5 +73,6 @@
 @endsection
 
 @section('script')
-{!! $dataTable->scripts() !!}
+    {!! $dataTable->scripts() !!}
+    <script src="{{asset('assets/js/datatable-filter.js')}}"></script>
 @endsection
