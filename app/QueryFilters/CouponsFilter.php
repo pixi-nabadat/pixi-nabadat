8b2de8670e114a0291d61fb9662d62d4ed561021
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-04-26 00:49:27 +0200

    coupons filters

--- app/QueryFilters/CouponsFilter.php
+++ app/QueryFilters/CouponsFilter.php
@@ -20,6 +20,46 @@ class CouponsFilter extends QueryFilter
         return $this->builder->where('is_active',$term);
     }
 
+    public function coupon_for($term)
+    {
+        return $this->builder->where('coupon_for',$term);
+    }
+
+    public function start_date($term)
+    {
+        return $this->builder->where('start_date',$term);
+    }
+
+    public function end_date($term)
+    {
+        return $this->builder->where('end_date',$term);
+    }
+
+    public function allowed_usage($term)
+    {
+        return $this->builder->where('allowed_usage',$term);
+    }
+
+    public function min_buy($term)
+    {
+        return $this->builder->where('min_buy',$term);
+    }
+
+    public function discount($term)
+    {
+        return $this->builder->where('discount',$term);
+    }
+
+    public function added_by($term)
+    {
+        return $this->builder->where('added_by',$term);
+    }
+
+    public function code($term)
+    {
+        return $this->builder->where('code',$term);
+    }
+
     public function in_period()
     {
         $date = Carbon::now(config('app.africa_timezone'))->format('Y-m-d');
