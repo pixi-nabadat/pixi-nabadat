1f1a7eb530dcfb293a404f4b22924331c88b5a94
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-04-25 23:12:10 +0200

    doctors filters

--- app/QueryFilters/DoctorsFilter.php
+++ app/QueryFilters/DoctorsFilter.php
@@ -22,19 +22,19 @@ class DoctorsFilter extends QueryFilter
         return $this->builder->where('name',$term);
     }
 
-    public function location_id($term)
-    {
-        return $this->builder->where('location_id',$term);
-    }
-
     public function center_id($term)
     {
         return $this->builder->where('center_id',$term);
     }
 
-    // public function center_id($term)
-    // {
-    //     return $this->builder->where('center_id',$term);
-    // }
+    public function phone($term)
+    {
+        return $this->builder->where('phone', $term);
+    }
+
+    public function age($term)
+    {
+        return $this->builder->where('age', $term);
+    }
 
 }
