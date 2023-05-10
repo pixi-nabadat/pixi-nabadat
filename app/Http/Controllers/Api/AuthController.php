47bffd00a906a9d8f5053474dda9590d010bd256
eslam <elbadry693@gmail.com>
2023-05-10 11:53:54 +0300

    fix register request

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -38,11 +38,6 @@ class AuthController extends Controller
     public function register(RegisterRequest $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $data = $request->validated();
-        $data = array_merge($data, ['type' => User::CUSTOMERTYPE, 'last_login_at' => now()]);
-        $data['name'] = [
-            'en' => $data['name'],
-            'ar' => $data['name'],
-        ];
         $data['password'] = bcrypt($data['password']);
         $result = $this->authService->register(data: $data);
         if ($result)
