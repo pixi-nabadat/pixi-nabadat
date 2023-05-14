014177316918049b3523a9acda1f4dc30669c06f
eslam <elbadry693@gmail.com>
2023-05-15 00:49:33 +0300

    fix general bugs

--- app/Http/Resources/product/ProductResource.php
+++ app/Http/Resources/product/ProductResource.php
@@ -26,7 +26,7 @@ class ProductResource extends JsonResource
             'rate'                      =>$this->rate,
             'feedback'                  =>$this->whenLoaded('rates',RatesResource::collection($this->rates)),
             'price_after_discount'      =>getPriceAfterDiscount($this->unit_price,$this->product_discount),
-            'images'                    => $this->attachments->isNotEmpty() ? AttachmentsResource::collection($this->attachments) : $this->image_path,
+            'images'                    => $this->attachments->isNotEmpty() ? AttachmentsResource::collection($this->attachments) : ['path'=>$this->image_path],
         ];
     }
 }
