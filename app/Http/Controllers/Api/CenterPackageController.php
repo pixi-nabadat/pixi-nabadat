76ccf58b63f6666ae1a20552bb11db50e62f70db
eslam <elbadry693@gmail.com>
2023-05-16 13:58:47 +0300

    fix packages resource

--- app/Http/Controllers/Api/CenterPackageController.php
+++ app/Http/Controllers/Api/CenterPackageController.php
@@ -21,7 +21,7 @@ class CenterPackageController extends Controller
     {
         try {
             $filters = ['is_active' => 1, 'in_duration' => true , 'status' =>PackageStatusEnum::APPROVED];
-            $withRelations = ['center.defaultLogo','attachments'];
+            $withRelations = ['attachments'];
             $allPackages = $this->packageService->listing(where_condition: $filters, withRelation: $withRelations);
             return PackagesResource::collection($allPackages);
         } catch (\Exception $exception) {
