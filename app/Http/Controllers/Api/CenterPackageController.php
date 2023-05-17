6831f30e8042b4e5ce91590084a1e1290f5d48e7
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-05-17 23:05:09 +0200

    hot fix packages listing api

--- app/Http/Controllers/Api/CenterPackageController.php
+++ app/Http/Controllers/Api/CenterPackageController.php
@@ -7,9 +7,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PackageStoreRequestApi;
 use App\Http\Requests\PackageUpdateRequest;
 use App\Http\Resources\PackagesResource;
+use App\Models\User;
 use App\Services\CenterPackageService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CenterPackageController extends Controller
 {
@@ -21,8 +23,12 @@ class CenterPackageController extends Controller
     public function index(Request $request): \Illuminate\Http\Response|\Illuminate\Http\Resources\Json\AnonymousResourceCollection|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         try {
-            $filters = $request->all();
-            $filters = ['is_active' => 1, 'in_duration' => true , 'status' =>PackageStatusEnum::APPROVED];
+            $filters = [] ;
+            $user = auth()->user(); 
+            if (isset($user) && $user->type == User::CENTERADMIN)
+                $filters['center_id'] = $user->center_id ; 
+            if(isset($user) && $user->type == User::CUSTOMERTYPE)
+                $filters = ['is_active' => 1, 'in_duration' => true , 'status' =>PackageStatusEnum::APPROVED];
             $withRelations = ['attachments'];
             $allPackages = $this->packageService->listing(where_condition: $filters, withRelation: $withRelations);
             return PackagesResource::collection($allPackages);
