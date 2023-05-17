5eda4a6c16408176d5cb2b82e8053046842b0f75
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-05-17 23:09:29 +0200

    bug fix in device resource

--- app/Http/Resources/DeviceResource.php
+++ app/Http/Resources/DeviceResource.php
@@ -15,11 +15,12 @@ class DeviceResource extends JsonResource
      */
     public function toArray($request)
     {
+        $logo = $this->attachments->where('type', ImageTypeEnum::LOGO)->first();
         return [
             'id'            => $this->id,
             'name'          => $this->name,
             'description'   => $this->description,
-            'logo'          => $this->whenLoaded('attachments',  new AttachmentsResource($this->attachments->where('type',ImageTypeEnum::LOGO)->first())),
+            'logo'          => isset($logo) ? asset($logo->path."/".$logo->filename):asset('assets/images/default-image.jpg'),
             'images'        => $this->whenLoaded('attachments', AttachmentsResource::collection($this->attachments->where('type',ImageTypeEnum::GALARY))),
         ];
     }
