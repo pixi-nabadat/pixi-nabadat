2bec7573cf92fa152384e67da36f37f60f9ee2e4
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-07-16 22:54:59 +0200

    send to fcm tokens

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\NotificationMarkAsReadRequest;
 use App\Http\Resources\NotifcationsResource;
 use App\Services\PushNotificationService;
-
+use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
@@ -36,5 +36,17 @@ class NotificationController extends Controller
     }
 
 
-//    send fcm to token
+    //    send fcm to token
+    public function sendFcmToToken(Request $request)
+    {
+        try {
+            $title = $request->title;
+            $body = $request->body;
+            $tokens = $request->device_tokens;
+            app()->make(PushNotificationService::class)->sendToTokens(title: $title,body: $body,tokens: $tokens);
+            return apiResponse(message: trans('lang.success_operation'));
+        } catch (\Exception $exception) {
+            return apiResponse(message: $exception->getMessage(), code: 400);
+        }
+    }
 }
