91c1897b0aa7a66137742dc7d37966c99f1b1d10
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-11-23 01:05:29 +0200

    add expire status in userPackage migration

--- database/migrations/2022_10_31_190813_create_user_packages_table.php
+++ database/migrations/2022_10_31_190813_create_user_packages_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->double('discount_percentage')->default(0);
             $table->enum('payment_method',[PaymentMethodEnum::CASH,PaymentMethodEnum::CREDIT]);
             $table->enum('payment_status',[PaymentStatusEnum::PAID,PaymentStatusEnum::UNPAID]);
-            $table->enum('status',[\App\Enum\UserPackageStatusEnum::PENDING,\App\Enum\UserPackageStatusEnum::READYFORUSE,\App\Enum\UserPackageStatusEnum::ONGOING,\App\Enum\UserPackageStatusEnum::COMPLETED]);
+            $table->enum('status',[\App\Enum\UserPackageStatusEnum::PENDING,\App\Enum\UserPackageStatusEnum::READYFORUSE,\App\Enum\UserPackageStatusEnum::ONGOING,\App\Enum\UserPackageStatusEnum::COMPLETED,\App\Enum\UserPackageStatusEnum::EXPIRED]);
             $table->integer('used')->default(0);
             $table->integer('remain')->default(0);
             $table->date('expire_date')->nullable();
