3b3706292246f4cf840fea5d071a5b51cef310ed
pro-mohamed-hamada <118569098+pro-mohamed-hamada@users.noreply.github.com>
2023-12-14 13:23:07 +0200

    bug fix packages search api

--- app/Http/Resources/HomeSearchResource.php
+++ app/Http/Resources/HomeSearchResource.php
@@ -19,7 +19,7 @@ class HomeSearchResource extends JsonResource
            'title' =>$this->getTranslation('name', app()->getLocale()),
            'type'  => $this->search_flag,
            'type_text'  => $this->search_flag_text,
-           'center_id'  => $this->whenLoaded('center', $this->center, null),
+           'center_id'  => $this->whenLoaded('center', $this->search_flag == 3 ? $this->center : [$this->center], null),
        ];
     }
 }
